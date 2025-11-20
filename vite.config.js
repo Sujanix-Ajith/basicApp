3e1e3d7fd06ed15a725c988f4f00ca17529a2314
Sujanix-Ajith <ajith@sujanix.com>
2025-11-20 06:46:01 +0530

    4 commits

--- vite.config.js
+++ vite.config.js
@@ -2,18 +2,18 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// export default defineConfig({
+//   plugins: [react()],
+// })
 
 
 // If using React Router, add this to vite.config.js to avoid 404s:
 
-// export default defineConfig({
-//   plugins: [react()],
-//   server: { port: 3000 },
-//   build: {
-//     outDir: 'dist'
-//   }
-// })
+export default defineConfig({
+  plugins: [react()],
+  server: { port: 3000 },
+  build: {
+    outDir: 'dist'
+  }
+})
 
